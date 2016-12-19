73db58a23f8897eafd0a377e11f54aa1c8039266
Brant Olsen <brant.olsen@jacquette.com>
2016-12-19 13:53:40 -0500

    Add initial scripts.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dota2Bots
 Dota 2 Bot Scripts
+
+- Clone to C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\scripts\vscripts\bots
---
+++ mode_laning_rattletrap.lua
@@ -0,0 +1,57 @@
+
+----------------------------------------------------------------------------------------------------
+
+function OnStart()
+	print("Start Clock Mode Laning");
+end
+
+----------------------------------------------------------------------------------------------------
+
+function OnEnd()
+	-- Do the standard OnEnd
+	print("End Clock Mode Laning");
+end
+
+----------------------------------------------------------------------------------------------------
+
+function Think()
+	local npcBot = GetBot();
+
+	-- Do the standard Think
+	mode_generic_defend_ally.Think()
+
+	-- Check if we're already using an ability
+	if ( npcBot:IsUsingAbility() ) then return end;
+	
+	-- If we have a target and can cast LSA on them, do so
+	if ( npcBot:GetTarget() ~= nil ) then
+		abilityLSA = npcBot:GetAbilityByName( "lina_light_strike_array" );
+		if ( abilityLSA:IsFullyCastable() )
+		then
+			npcBot:Action_UseAbilityOnLocation( abilityLSA, npcBot:GetTarget():GetLocation() );
+		end
+	end
+end
+
+----------------------------------------------------------------------------------------------------
+
+function GetDesire()
+
+	local npcBot = GetBot();
+	local fBonus = 0.0;
+
+	-- If we have a target and can cast Light Strike Array, our desire to help defend should be higher than normal
+	if ( npcBot:GetTarget() ~= nil )
+	then
+		abilityLSA = npcBot:GetAbilityByName( "lina_light_strike_array" );
+		if ( abilityLSA:IsFullyCastable() )
+		then
+			fBonus = 0.25;
+		end
+	end
+
+	return Clamp( mode_generic_defend_ally.GetDesire() + fBonus, BOT_MODE_DESIRE_NONE, BOT_MODE_DESIRE_ABSOLUTE );
+end
+
+----------------------------------------------------------------------------------------------------
+
